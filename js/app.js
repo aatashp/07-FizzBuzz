13f5e2d2b5f0a64c22f9b3e966f5972937153391
Aatash <aatashp@gmail.com>
2014-05-27 07:59:36 -0400

    Refactored to accept number input

--- js/app.js
+++ js/app.js
@@ -1,40 +1,95 @@
 $('document').ready(function() {
 
 	// VARIABLES
-	var fizz = "Fizz",
-			buzz = "Buzz",
-			list = "#q1",
-			output = "";
+	var runFizzBuzz = "#run-fizzbuzz",
+			numInput = "#num-input",
+			msg = "#msg",
+			columns = "#q1, #q2, #q3, #q4",
+			num = ".num",
+			lists = "#lists",
+			keyNumber = 13;
 
 	// FUNCTIONS
+	var fizzBuzz = function() {
 
+		var column = "#q1",
+				fizz = "Fizz",
+				buzz = "Buzz",
+				output = "",
+				input = $(numInput).val(),
+				message = "";
 
-	// INITIALIZE
-	for (i=1; i<=100; i++) {
-		output = "";
+		//Reset
+		$(columns).hide();
+		$(num).remove();
+		$(msg).html("");
 
-		if(i%3==0) {
-			output = fizz;
-		} 
+		message = inspectInput(input);
 
-		if (i%5==0) {
-			output += buzz;
-		} 
+		if (message.length == 0) {
 
-		if (output.length == 0) {
-			output = i;
-		}
+			input = +input;
+			$(lists).hide().fadeIn('slow');
 
-		output = "<p>" + output	+ "</p>";
+			for (i=1; i<=input; i++) {
+				output = "";
 
-		$(list).append(output).hide().fadeIn();
+				$(column).show();
 
-		if (i%25==0) {
-			list = "#q" + (i/25 + 1);
-			console.log(list);
+				if(i%3==0) {
+					output = fizz;
+				} 
+
+				if (i%5==0) {
+					output += buzz;
+				} 
+
+				if (output.length == 0) {
+					output = i;
+				}
+
+				output = "<p class='num'>" + output	+ "</p>";
+
+				$(column).append(output);
+
+				if (i%25==0) {
+					column = "#q" + (i/25 + 1);
+				}
+			}
+		} else {
+			$(msg).html(message);
+			$("#lists").hide();
 		}
 	}
 
-	// EVENT HANDLERS
 
+	var inspectInput = function(input) {
+		var erromsg = "Please enter a number between 1 and 100";
+
+		$(numInput).val('');
+
+		if (isNaN(input)) {
+			return erromsg;
+		} else if (input%1 != 0) {
+			return erromsg;
+		} else if ( (input < 1) || (input > 100) ) {
+			return erromsg;
+		} else {
+			return "";
+		}
+
+	}	
+
+	function keyPressed(event) {
+		if (event.which == keyNumber) {
+			fizzBuzz();
+		}
+	}
+
+
+	// INITIALIZE
+
+	// EVENT HANDLERS
+	$(document).on('keydown', keyPressed);
+	$(runFizzBuzz).on('click', fizzBuzz);
 })
